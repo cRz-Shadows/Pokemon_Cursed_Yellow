2b1584e9057d36d15bd0fabaa2f6caf66182eb08
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-03-31 20:36:00 +0100

    mewtwo amnesia

--- data/pokemon/base_stats/mewtwo.asm
+++ data/pokemon/base_stats/mewtwo.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/mewtwo.pic", 0, 1 ; sprite dimensions
 	dw MewtwoPicFront, MewtwoPicBack
 
-	db SPLASH, DISABLE, REST, GROWTH ; level 1 learnset
+	db SPLASH, AMNESIA, REST, GROWTH ; level 1 learnset
 	db GROWTH_SLOW ; growth rate
 
 	; tm/hm learnset
